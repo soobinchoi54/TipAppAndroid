562f8461457870e1438aef2c148a00be29c8ae45
Soobin Choi <soobinchoi54@gmail.com>
2020-02-25 17:23:30 -0800

    restore

---
+++ app/src/main/java/com/example/myapplication/DataBaseHelper.java
@@ -0,0 +1,151 @@
+package com.example.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+/************************************************************************************************
+ * Database System that provides API for storing all the experience records for further usages
+ ************************************************************************************************/
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "TipsDataBase";
+    public static final int DB_VERSION = 2;
+
+    DataBaseHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        updateMyDatabase(db,0, DB_VERSION);
+        System.out.print("Database created");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS TipsDataBase");
+        db.execSQL("CREATE TABLE TipsDataBase (_id INTEGER PRIMARY KEY AUTOINCREMENT, " + "NAME TEXT, "
+                + "LOCATION TEXT, "
+                + "CATEGORY TEXT, "
+                + "PRICE TEXT, "
+                + "TOTAL_BILL TEXT, "
+                + "TIP_PERCENTAGE TEXT, "
+                + "TIME TEXT, "
+                + "CRITERIA1 TEXT, "
+                + "CRITERIA2 TEXT, "
+                + "CRITERIA3 TEXT, "
+                + "CUSTOM TEXT, "
+                + "CUSTOM_RATE TEXT, "
+                + "IMAGE TEXT);");
+    }
+
+    // add an experience to database (inner logic)
+    private static void add(SQLiteDatabase db, String NAME, String LOCATION, String CATEGORY, String PRICE, String TOTAL_BILL, String TIP_PERCENTAGE, String TIME, String CRITERIA1, String CRITERIA2, String CRITERIA3, String CUSTOM, String CUSTOM_RATE, String IMAGE){
+        ContentValues experience = new ContentValues();
+        experience.put("NAME", NAME);
+        experience.put("LOCATION", LOCATION);
+        experience.put("CATEGORY", CATEGORY);
+        experience.put("PRICE", PRICE);
+        experience.put("TOTAL_BILL", TOTAL_BILL);
+        experience.put("TIP_PERCENTAGE", TIP_PERCENTAGE);
+        experience.put("TIME", TIME);
+        experience.put("CRITERIA1", CRITERIA1);
+        experience.put("CRITERIA2", CRITERIA2);
+        experience.put("CRITERIA3", CRITERIA3);
+        experience.put("CUSTOM", CUSTOM);
+        experience.put("CUSTOM_RATE", CUSTOM_RATE);
+        experience.put("IMAGE", IMAGE);
+        db.insert(DB_NAME, null, experience);
+        System.out.println("Record Written");
+    }
+
+    // query experiences and return a List
+    private static List<Experience> get(SQLiteDatabase db){
+        List<Experience> experiences = new ArrayList<>();
+        boolean firstData = true;
+        try {
+            Cursor cursor = db.query (DB_NAME,
+                    new String[] {"NAME", "LOCATION", "CATEGORY","PRICE", "TOTAL_BILL", "TIP_PERCENTAGE", "TIME", "CRITERIA1", "CRITERIA2", "CRITERIA3", "CUSTOM", "CUSTOM_RATE", "IMAGE"},
+                    null,
+                    null,
+                    null, null, null);
+            System.out.println("Size: " + cursor.getCount());
+            if (cursor.moveToFirst()){
+                System.out.println("Got data");
+                while (firstData || cursor.moveToNext()){
+                    String NAME = cursor.getString(0);
+                    String LOCATION = cursor.getString(1);
+                    String CATEGORY = cursor.getString(2);
+                    String PRICE = cursor.getString(3);
+                    String TOTAL_BILL = cursor.getString(4);
+                    String TIP_PERCENTAGE = cursor.getString(5);
+                    String TIME = cursor.getString(6);
+                    String CRITERIA1 = cursor.getString(7);
+                    String CRITERIA2 = cursor.getString(8);
+                    String CRITERIA3 = cursor.getString(9);
+                    String CUSTOM = cursor.getString(10);
+                    String CUSTOM_RATE = cursor.getString(11);
+                    String IMAGE = cursor.getString(12);
+                    experiences.add(new Experience(NAME, LOCATION, CATEGORY, PRICE, TOTAL_BILL, TIP_PERCENTAGE, TIME, CRITERIA1, CRITERIA2, CRITERIA3, CUSTOM, CUSTOM_RATE, IMAGE));
+                    firstData = false;
+                }
+            }
+        } catch (SQLiteException e){
+            throw(e);
+        }
+        return experiences;
+    }
+
+    // API: add an experience to database
+    public static void addExperience(Context context, String NAME, String LOCATION, String CATEGORY, String PRICE, String TOTAL_BILL, String TIP_PERCENTAGE, String TIME, String CRITERIA1, String CRITERIA2, String CRITERIA3, String CUSTOM, String CUSTOM_RATE, String IMAGE){
+        SQLiteOpenHelper dataBaseHelper = new DataBaseHelper(context);
+        SQLiteDatabase db = dataBaseHelper.getWritableDatabase();
+        try {
+            DataBaseHelper.add(db, NAME, LOCATION, CATEGORY, PRICE, TOTAL_BILL, TIP_PERCENTAGE, TIME, CRITERIA1, CRITERIA2, CRITERIA3, CUSTOM, CUSTOM_RATE, IMAGE);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(context, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    // API: retrieve experiences from database
+    public static List<Experience> getExperiences(Context context){
+        SQLiteOpenHelper dataBaseHelper = new DataBaseHelper(context);
+        SQLiteDatabase db = dataBaseHelper.getReadableDatabase();
+        List<Experience> experiences = new ArrayList<>();
+        try {
+            experiences = DataBaseHelper.get(db);
+        } catch (SQLiteException e){
+            Toast toast = Toast.makeText(context, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return experiences;
+    }
+
+    // API: delete all data from database
+    public static void deleteTable(Context context){
+        SQLiteOpenHelper dataBaseHelper = new DataBaseHelper(context);
+        SQLiteDatabase db = dataBaseHelper.getWritableDatabase();
+        try {
+            db.execSQL("delete from TipsDataBase");
+            System.out.println("Database Table deleted");
+        } catch (SQLiteException e){
+            Toast toast = Toast.makeText(context, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public static void testAdd2Experience(Context context){
+        addExperience(context, "BCD", "Irvine", "Korean", "$$", "25$", "15%", "30:00", "-1%", "0%", "+1%", "Good Restroom#Not enough fish cake  ", "+1%#-1%", "https://i.imgur.com/DvpvklR.png");
+        addExperience(context, "HaiDiLao", "Irvine", "Chinese", "$$$", "100$", "15%", "50:00", "-1%", "0%", "+1%", "Good restroom#Good sneak", "+1%#+1%", "https://i.imgur.com/DvpvklR.png");
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/HistoryListAdapter.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+/**************************************************************************************************
+ *              Adapter class for ViewHistoryActivity.java
+ ***************************************************************************************************/
+public class HistoryListAdapter extends RecyclerView.Adapter<HistoryListAdapter.ViewHolder> {
+    private List<Experience> experiences;
+    public HistoryListAdapter(List<Experience> experiences) { this.experiences = experiences; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivRestaurant;
+        TextView tvName;
+        TextView tvLocation;
+        TextView tvCategory;
+        TextView tvPrice;
+        TextView tvTotalBill;
+        TextView tvTipPercentage;
+        TextView tvTime;
+        TextView tvCritera1;
+        TextView tvCritera2;
+        TextView tvCritera3;
+        TextView tvCustom;
+
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ivRestaurant = itemView.findViewById(R.id.ivRestaurant);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+            tvCategory = itemView.findViewById(R.id.tvCategory);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            tvTotalBill = itemView.findViewById(R.id.tvTotalBill);
+            tvTipPercentage = itemView.findViewById(R.id.tvTipPercentage);
+            tvTime = itemView.findViewById(R.id.tvTime);
+            tvCritera1 = itemView.findViewById(R.id.tvCritera1);
+            tvCritera2 = itemView.findViewById(R.id.tvCritera2);
+            tvCritera3 = itemView.findViewById(R.id.tvCritera3);
+            tvCustom = itemView.findViewById(R.id.tvCustom);
+
+        }
+    }
+
+    public void setData(List<Experience> experiences){
+        this.experiences = experiences;
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public HistoryListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.experience_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryListAdapter.ViewHolder holder, int position) {
+        Experience experience = experiences.get(position);
+        holder.tvName.setText(experience.getName());
+        holder.tvLocation.setText("Location: " + experience.getLocation());
+        holder.tvCategory.setText("Category: " + experience.getCategory());
+        holder.tvPrice.setText("Price: " + experience.getPrice());
+        holder.tvTotalBill.setText("Total Bill: " + experience.getTotalBill());
+        holder.tvTipPercentage.setText("Tip Percentage: " + experience.getTipPercentage());
+        holder.tvTime.setText("Time spent: " + experience.getTime());
+        holder.tvCritera1.setText("Critera1: " + experience.getCriteria1());
+        holder.tvCritera2.setText("Critera2: " + experience.getCriteria2());
+        holder.tvCritera3.setText("Critera3: " + experience.getCriteria3());
+        String[] customs = experience.getCustom().split("#");
+        String[] custom_rates = experience.getCUSTOM_RATE().split("#");
+        StringBuilder customBuilder = new StringBuilder();
+        for(int i = 0; i < customs.length; i++){
+            customBuilder.append(customs[i].trim() + ": " + custom_rates[i].trim() + "\n");
+        }
+        String custom = customBuilder.toString();
+        holder.tvCustom.setText(custom);
+        Picasso.get().load(experience.getImage()).into(holder.ivRestaurant);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return experiences.size();
+    }
+
+}
